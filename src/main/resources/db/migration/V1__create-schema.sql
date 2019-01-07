039b296cadef89c058eca98741dadb095c4a5dbf
Manabu KAWAKAMI <kawakami-ma@itscom.jp>
2019-01-07 15:19:55 +0900

    Update V1__create-schema.sql

--- src/main/resources/db/migration/V1__create-schema.sql
+++ src/main/resources/db/migration/V1__create-schema.sql
@@ -28,3 +28,33 @@ CREATE TABLE `comments` (
   CONSTRAINT `FK_COMMENTS_REPORTID` FOREIGN KEY (`report_id`) REFERENCES `REPORTS` (`report_id`) ON DELETE CASCADE,
   CONSTRAINT `FK_COMMENTS_STAFFID` FOREIGN KEY (`staff_id`) REFERENCES `STAFFS` (`staff_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE VIEW report_status AS 
+SELECT
+ s0.staff_id
+ ,s0.name
+ ,r0.report_id
+ ,r0.last_reported
+ ,r0.report
+ ,r0.last_commented
+ ,r0.last_commented_by
+FROM
+ staffs s0 LEFT JOIN (
+SELECT
+ r1.report_id
+ ,r1.staff_id
+ ,r1.reported_when AS last_reported
+ ,r1.report
+ ,c1.commented_when AS last_commented
+ ,s2.name AS last_commented_by
+FROM
+ reports r1 
+ LEFT JOIN staffs s1 ON r1.staff_id=s1.staff_id
+ LEFT JOIN comments c1 LEFT JOIN staffs s2 ON c1.staff_id=s2.staff_id ON r1.report_id=c1.report_id
+WHERE
+ r1.reported_when = (SELECT max(reported_when) FROM reports r2 WHERE r1.staff_id=r2.staff_id)
+ AND (c1.commented_when = (SELECT max(commented_when) FROM comments c2 WHERE r1.report_id=c2.report_id) OR c1.commented_when IS null) 
+ ) r0 ON s0.staff_id=r0.staff_id
+WHERE
+role=0
+ORDER BY s0.staff_id;
