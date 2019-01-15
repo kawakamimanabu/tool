d9332b00f9f05d83b9500179621e3ac47ee6e3d9
Manabu KAWAKAMI <kawakami-ma@itscom.jp>
2019-01-15 18:00:05 +0900

    updated

--- src/main/java/com/dev/edu/tool/domain/Subscriber.java
+++ src/main/java/com/dev/edu/tool/domain/Subscriber.java
@@ -3,20 +3,28 @@ package com.dev.edu.tool.domain;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "subscribers")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subscriber {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer notificationId;
+  private Integer subscriberId;
   
   @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(nullable = false, name = "notificationId")
--- src/main/java/com/dev/edu/tool/repository/SubscriberRepository.java
+++ src/main/java/com/dev/edu/tool/repository/SubscriberRepository.java
@@ -6,11 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.dev.edu.tool.domain.Notification;
 import com.dev.edu.tool.domain.Subscriber;
 
 public interface SubscriberRepository extends JpaRepository<Subscriber, Integer> {
-  @Query("SELECT s FROM Subscriber s WHERE s.notificationId=:notificationId ORDER BY s.staff.staffId ASC")
-  List<Notification> findAllByNotificationId(@Param("notificationId") Integer notificationId);
+  @Query("SELECT s FROM Subscriber s WHERE s.notification.notificationId=:notificationId ORDER BY s.staff.staffId ASC")
+  List<Subscriber> findAllByNotificationId(@Param("notificationId") Integer notificationId);
   
 }
